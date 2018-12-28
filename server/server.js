86ed98b35eace3ab09c903ff26f6755e9866ccb9
uros87 <uros_brkic@yahoo.com>
2018-12-28 11:27:17 +0100

    as

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const express = require('express');
+
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express(app);
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
