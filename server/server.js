d1965e26cb0cd43487aa4bcb35f32cfed7981760
uros87 <uros_brkic@yahoo.com>
2019-01-08 09:50:52 +0100

    ada

--- server/server.js
+++ server/server.js
@@ -1,16 +1,37 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-var app = express(app);
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
+					
+io.on('connection', (socket) => {
+	console.log('New user connected');
 
-app.listen(port, () => {
+
+	socket.on('createMessage', (message) => {
+		console.log('Create message', message);
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		});
+	});
+
+	socket.on('disconnect', () => {
+		console.log('User disconnected')
+	});
+});
+
+server.listen(port, () => {
 	console.log(`Server is up on port ${port}`)
-})
+});
 
 
 
