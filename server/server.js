c286b36f6858dc5bbd0c6f34750723beb1fb3d84
uros87 <uros_brkic@yahoo.com>
2019-01-08 12:38:09 +0100

    da

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,14 +16,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-	socket.on('createMessage', (message) => {
+	socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+	socket.on('createMessage', (message, callback) => {
 		console.log('Create message', message);
-		io.emit('newMessage', {
-			from: message.from,
-			text: message.text,
-			createdAt: new Date().getTime()
-		});
+		io.emit('newMessage', generateMessage(message.from, message.text));
+		callback('This is from the server');
+		// socket.broadcast.emit('newMessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createdAt: new Date().getTime()
+		// })
 	});
 
 	socket.on('disconnect', () => {
