e3e0a1298cc2768f7335d1ccf76180d632a75801
uros87 <uros_brkic@yahoo.com>
2019-01-11 16:04:13 +0100

    asa

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
 	it('should generate correct message object', () => {
@@ -12,5 +12,18 @@ describe('generateMessage', () => {
 			from: from,
 			text: text
 		})
+	});
+});
+
+describe('generateLocationMessage', () => {
+	it('should generate correct location object', () => {
+		var from = 'Deb';
+		var latitude = 15;
+		var longitude = 19;
+		var url = 'https://www.google.com/maps?q=15,19';
+		var message = generateLocationMessage(from, latitude, longitude);
+		
+		expect(message.createdAt).toBeA('number');
+		expect(message).toInclude({})
 	});
 });
